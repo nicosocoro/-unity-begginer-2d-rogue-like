9f0568cff8ab543709dbd12a29f035afbea8e151
Nicolas Socoro <nicolassocoro@gmail.com>
2025-08-31 23:34:08 -0300

    Player: Animate when Idle and Walking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,30 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     private BoardManager _board;
     private Vector2Int _currentPosition;
+    private Vector2Int? _targetPosition = null;
+    private Animator _animator;
+    private bool _isMoving = false;
 
     public event Action<Vector2Int> OnPlayerMoved;
     public event Action OnRestartRequested;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     public void Spawn(BoardManager boardManager, Vector2Int cell)
     {
         _board = boardManager;
         _currentPosition = cell;
+        _isMoving = false;
+        _targetPosition = null;
+        _animator.SetBool("Moving", false);
 
         transform.position = _board.GetCellPosition(_currentPosition);
     }
@@ -22,6 +34,7 @@ public class PlayerController : MonoBehaviour
     {
         if (GameManager.Instance.IsGameActive)
         {
+            HandleIsMoving();
             HandleInputInActiveGame();
         }
         else
@@ -67,7 +80,7 @@ public class PlayerController : MonoBehaviour
 
     private void TryMoveTo(Vector2Int targetCell)
     {
-        if (_board.IsCellAvailableForPlayer(targetCell))
+        if (!_isMoving && _board.IsCellAvailableForPlayer(targetCell))
         {
             MoveTo(targetCell);
             OnPlayerMoved?.Invoke(targetCell);
@@ -76,7 +89,29 @@ public class PlayerController : MonoBehaviour
 
     private void MoveTo(Vector2Int targetCell)
     {
+        _targetPosition = targetCell;
+        _isMoving = true;
+        _animator.SetBool("Moving", true);
+
         _currentPosition = targetCell;
-        transform.position = _board.GetCellPosition(_currentPosition);
+    }
+
+    private void HandleIsMoving()
+    {
+        if (_isMoving && _targetPosition != null)
+        {
+            Vector3 targetWorldPos = _board.GetCellPosition(_targetPosition.Value);
+            if (Vector3.Distance(transform.position, targetWorldPos) < 0.01f)
+            {
+                transform.position = targetWorldPos;
+                _isMoving = false;
+                _targetPosition = null;
+                _animator.SetBool("Moving", false);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetWorldPos, Time.deltaTime * 5f);
+            }
+        }
     }
 }
